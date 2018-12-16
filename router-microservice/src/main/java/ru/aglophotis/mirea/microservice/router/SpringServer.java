d0b60f761d015bbd0e70847503ff2a4739a76bc1
Aglophotis <atisphatis@gmail.com>
2018-12-16 23:48:49 +0300

    Added router

--- router-microservice/src/main/java/ru/aglophotis/mirea/microservice/router/SpringServer.java
+++ router-microservice/src/main/java/ru/aglophotis/mirea/microservice/router/SpringServer.java
@@ -1,14 +1,16 @@
-package  ru.aglophotis.mirea.microservice.item.shop;
+package ru.aglophotis.mirea.microservice.router;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 
 @SpringBootApplication
+@EnableZuulProxy
 public class SpringServer {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(SpringServer.class);
-        app.run(args);
+        SpringApplication.run(SpringServer.class, args);
     }
 
 }
