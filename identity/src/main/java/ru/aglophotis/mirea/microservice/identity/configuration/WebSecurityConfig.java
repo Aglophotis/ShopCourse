a670852765591850800292c4d25eced93952dfc6
Aglophotis <atisphatis@gmail.com>
2018-12-22 01:52:51 +0300

    Small features

---
+++ identity/src/main/java/ru/aglophotis/mirea/microservice/identity/configuration/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package ru.aglophotis.mirea.microservice.identity.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/user").permitAll()
+                .antMatchers(HttpMethod.PUT, "/user").permitAll()
+                .antMatchers(HttpMethod.POST, "/getUser").permitAll()
+                .anyRequest().authenticated();
+    }
+}
---
+++ item-microservice/src/main/java/ru/aglophotis/mirea/microservice/item/configuration/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package ru.aglophotis.mirea.microservice.item.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf()
+                .disable()
+                .exceptionHandling()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/item").permitAll()
+                .antMatchers(HttpMethod.GET, "/item/*").permitAll()
+                .antMatchers(HttpMethod.POST, "/item/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/pet").permitAll()
+                .antMatchers(HttpMethod.GET, "/stuff").permitAll()
+                .anyRequest().authenticated();
+    }
+}
